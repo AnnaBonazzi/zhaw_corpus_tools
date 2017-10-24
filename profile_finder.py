e6c7ade007c210e87d9501ca673a17984843df50
AnnaBonazzi <30931222+AnnaBonazzi@users.noreply.github.com>
2017-10-24 17:41:18 +0200

    Update profile_finder.py

--- profile_finder.py
+++ profile_finder.py
@@ -2,70 +2,55 @@
 '''
 Anna Bonazzi, 06/09/2017
 
-Script to group corpus actors based on their use of collocations
+Script to group corpus actors based on their use of collocations. 
+Requires predefined collocation profiles (see line 32) in this format:
+	'economic field' : ['expensive', 'debit', 'money'], 'political field' : ['president', 'parliament', 'laws'], etc... 
 
 What the script does:
 	1) Picks corpus texts with chosen language and containing searchword. 
-	2) searches text chunks (ngram of given size) where searchword is.
-	3) Counts the frequency of every profile element. Sums them to obtain the global profile value for each source.
-	4) Counts relative frequency of profile words 8% of the frequency of searchword in that source)
+	2) Looks for text chunks (ngrams of given size) where the searchword is.
+	3) Counts the frequency of every collocate for every profile. Sums them to obtain the global profile value for each source.
+	4) Counts relative frequency of profile words (% of the frequency of searchword in that source)
 	5) Represents results as table where rows are sources and columns are profiles.
 
-        politique  verbes		Density
-acg            20       8		28
-agrige          0       0		0
-ibarau         16       8		24
+						economic  	political		Density
+acg					20					8						28
+agrige			0 					0						0
+ibarau			16					8						24
 
-Total				36			16
+Total				36					16
 
-! Requires all the encode/decode in python2.7. Comment it out for python 3.
+Current settings for python 2.7 (for python 3, comment all the ".decode('utf-8')" out).
 '''
 #--------------------------
 # VARIABLES FOR USER TO CHANGE:
 
-#searchword = 'genevois'
-#reg = '(géotherm.*?|genevoi.*)'
-#coll_profiles = {'Akteur' : ['SIG', 'CleanTechs', 'Geofortec', 'www.augsburgerforages.ch', 'www.stump.ch', 'Aproz', 'www.swissgeotesting.ch', 'www.beg-geol.ch', 'Augsburger', 'eury', 'SA', 'Sàrl'], 'Grüne Energie' : ['biomasse', 'solaire', 'hydraulique', 'éolien_,', 'éolien', 'd’eau', 'Geothermik', 'solaires', 'éolienne', 'photovoltaïque', 'gaz', 'chaleur'], 'Politique' : ['commune', 'Suisse', 'association', 'année']}
-
 searchword = 'géothermie'
 reg = 'géotherm.*' # Upper case / Lower case will be fixed later
-coll_profiles = {'Akteur' : ['SIG', 'CleanTechs', 'Geofortec', 'www.augsburgerforages.ch', 'www.stump.ch', 'Aproz', 'www.swissgeotesting.ch', 'www.beg-geol.ch', 'Augsburger', 'eury', '^SA$', 'Sàrl'], 
-'Energieressource' : ['biomasse', 'solaire', 'hydraulique', 'éolien', '’eau', 'photovoltaïque', '^gaz', '^thermique', 'hydro-thermique', 'biogaz', 'bois'], 
+coll_profiles = {'Energieressource' : ['biomasse', 'solaire', 'hydraulique', 'éolien', '’eau', 'photovoltaïque', '^gaz', '^thermique', 'hydro-thermique', 'biogaz', 'bois'], 
 'Grüne Energie' : ['biomasse', 'solaire', 'hydraulique', 'éolien', '’eau', 'photovoltaïque', 'gaz', 'chaleur'], 
-'Energiewirtschaft' : ['prospection', 'exploitation', 'utilisation', 'production', 'exploiter'], 
-'Offene Chance' : ['potentiel', 'promett.*', 'permettent'], 
-'Instrument' : ['panneaux', 'sonde', 'forage', 'pompe', 'installation', 'pompage', 'hydrothermale', 'centrale'], 
-'Person' : ['Brunier', 'Meyer', 'Barthassat', '^Luc', 'Michel'], 
-'Positive Eig. Energie' : ['renouvelable', 'régénèr'], 
-'Raum': ['profonde', 'moyenne', 'sous-sol', 'mètres', 'faible', 'zones'], 
-'Anlagenstandort' : ['Troistorrent', 'Lavey', 'd’Yvonand', 'Vinzel', 'Côte', 'Gall', 'Bains', 'Bâle', 'Basel'], 
-'Stadt' : ['Troistorrent','BÂLE', 'Basel', 'GALL', 'Vinzel', 'Lavey'], 
-'Verfahren' : ['mining', 'terrassement', 'pétrothermale', 'Fracturation'], 
-'Vorhaben' : ['projet', 'développement', 'développer', 'exploiter', '2020', 'programme'], 
-'Kanton' : ['VD', 'Vaud', 'BÂLE', 'Basel', 'GALL']}
+'Energiewirtschaft' : ['prospection', 'exploitation', 'utilisation', 'production', 'exploiter']}
 
-# Hint: When searching by regex, use "word". When searching by plain word, use "lemma".
 lang = 'fr'
-input_file = '/home/bonz/Documents/Corpora/geothermie_jul17.vrt'
-output_folder = '/home/bonz/Documents/Corpus_work/GEothermie2020/collocates/'
+
+input_file = '/path/to/corpus_file.vrt'
+output_folder = 'path/to/collocates_folder/'
 window_size = 6 # Size of word window to search for collocates
-unit = 'lemma'
+unit = 'lemma' # Use lemma if the collocates were obtained as lemmas, or wordform if collocates are wordforms
 #---------------------
 
 # To time the script
 from datetime import datetime
 startTime = datetime.now()
-
+import pandas as pd
 import os, glob, re, sys
 #---------------------
 
-# 1) Assembles text with chosen lang combination
+# 1) Looks for texts with chosen language containing the searchword
 			
 chunk = []
-sources = {}
-sources_percent = {}
+sources = {}; sources_percent = {}
 sw_counter = {}
-sw_freq = 0
 text_counter = 0
 switch = 0
 reg = reg.decode('utf-8')
@@ -92,12 +77,7 @@ with open (input_file, 'r') as f:
 				switch = 1
 				chunk = chunk[1:]
 				temp = []
-				for word in chunk:
-					# Counts searchword frequency
-					regex2 = re.search(reg, word)
-					if regex2:
-						sw_freq += 1
-						
+				for word in chunk:						
 					# Saves only ngrams where word is, shifting 1 by 1
 					while len(temp) < window_size:
 						temp.append(word)
@@ -115,20 +95,14 @@ with open (input_file, 'r') as f:
 								for profile in coll_profiles:
 									if profile not in sources[source]:
 										sources[source][profile] = 0
-									
 									# Checks for every collocate in profile
 									for coll in coll_profiles[profile]:
-										
 										collocate = re.search('(?i)'+coll.decode('utf-8'), ' '.join(temp))
 										if collocate:
-											
 											# Saves frequency of profile elements (multiplied in many ngrams - not real, but useful as proportion)
-											sources[source][profile] += 1
-											if source == 'ferge' and profile == ('Energieressource' or 'Grüne Energie'):	
-												print (' '.join(temp) + '\t-> ' + coll.decode('utf-8') + ' ' + profile.decode('utf-8') + ' ' + str(sources[source][profile]))
-											
+											sources[source][profile] += 1									
 										
-											
+						# Continues shifting right with new ngrams					
 						temp.pop(0)
 					else:
 						temp.pop(0)
@@ -148,52 +122,32 @@ else:
 		for profile in coll_profiles:
 			# Makes parallel dictionary with relative frequency
 			if sources[source][profile] > 0:
-				
 				rel_freq = float('{:.2f}'.format(float(sources[source][profile]) * 100 / sw_counter[source]))
-				print('\t' + profile + ' ' + str(sources[source][profile]) + ', rel fr ' + str(rel_freq))
 				sources_percent[source][profile] = rel_freq
 			else:
 				sources_percent[source][profile] = 0
-			
-	#print (sources)
-
-	# To do: think of how to filter the tables. What is the proportion? 
-	# Not all sources / Not all classes. What is the minimum limit?
-						
-	import pandas as pd
+	
+	# Puts results into a table
 	df = pd.DataFrame.from_dict(sources)
 	df = df.T # Swaps x and y
-
+	
 	df_percent = pd.DataFrame.from_dict(sources_percent)
 	df_percent = df_percent.T # Swaps x and y
 
-	# Sum single column values
-	#df.at['Total', 'politique'] = df['politique'].sum()
-
-	# Sum all column values, add row with column sum
+	# Sums all column values, adds row with column sum
 	for cl in coll_profiles.keys():
 		df.at['Profilrelevanz', cl] = df[cl].sum()
 		df_percent.at['Profilrelevanz', cl] = df_percent[cl].sum()
 	
-	# Sum all row values, add row with row sum
+	# Sums all row values, adds row with row sum
 	row_sum = df.sum(axis=1) # Type: series
 	df['Thematische Dichte'] = row_sum # Takes list or pandas series
 
 	row_sum = df_percent.sum(axis=1)
 	df_percent['Thematische Dichte'] = row_sum
 
-	# Other functions:
-
-	#print(df.describe())
-
-	#for index, row in df.iterrows():
-	#	print (row)
-
-
-	print('\n')
-	#print(df)
-	print('\n')
 	#print(df_percent)
+	
 	df.to_csv(output_folder+searchword+'_profiles3.csv', sep='\t')
 	df_percent.to_csv(output_folder+searchword+'_profiles_percent3.csv', sep='\t')
 #--------------------------
