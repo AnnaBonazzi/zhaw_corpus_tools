dd5410685620ec14b3653659c265e65bac1768b7
AnnaBonazzi <30931222+AnnaBonazzi@users.noreply.github.com>
2017-10-26 14:12:31 +0200

    Update bar_diagram_class.py

--- bar_diagram_class.py
+++ bar_diagram_class.py
@@ -2,13 +2,11 @@
 '''
 Anna Bonazzi, 16/08/2017
 
-Script to make a plot of corpus words frequency in each corpus class or language, depending on the format of the input data.
+Script to make a plot of corpus words frequency for each corpus class or language, depending on the format of the input data.
 
-Every keyword has its frequency dic with frequency per class. The classes could be replaced by language data.
+Every keyword has its frequency dic with frequency per class/language.
 
-For more / less words: add/remove a new keyword, a new dic, and a new "make_bar"-function at line 67
-
-For more / less bar groups: 
+For more / less words: add/remove a new keyword, a new dic, a new "make_bar"-function at line 75, and a new keyword in the 
 '''
 # VARIABLES FOR USER TO CHANGE
 
@@ -21,9 +19,11 @@ keyw6 = 'Éolien'.decode('utf-8')
 keyw7 = 'Biomasse'.decode('utf-8')	
 keyw8 = 'Hydroélectrique / hydraulique'.decode('utf-8')	
 
+legend_names = [keyw1, keyw2, keyw3, keyw4, keyw5, keyw6, keyw7, keyw8] # Add / remove for more / less words
+
 bar_colors = {keyw1 : '#17202a', keyw2 : '#a93226', keyw3 : '#f1c40f', keyw4 : '#717d7e', keyw5 : '#196f3d', keyw6 : '#aed6f1', keyw7 : '#7d6608', keyw8 : '#2874a6'} # Pick colors from http://htmlcolorcodes.com/
 
-number_of_bar_groups = 4 # For classes (4) or for languages
+number_of_bar_groups = 4 # 4 for 4 classes. Change for more / less bar groups, e.g. for 5 or more languages
 
 dic1 = {'pab': 856, 'pbv': 7495, 'peb': 8, 'pfu': 509}
 dic2 = {'pab': 394, 'pbv': 202, 'peb': 1, 'pfu': 172}
@@ -34,10 +34,11 @@ dic6 = {'pab': 576, 'pbv': 740, 'peb': 0, 'pfu': 108}
 dic7 = {'pab': 164, 'pbv': 54, 'peb': 5, 'pfu': 73}
 dic8 = {'pab': 1399, 'pbv': 482, 'peb': 20, 'pfu': 324}
 
-title = 'Energiesorten\nSubklassen'
+title = 'Energiesorten September Release \'17\nAkteursklassen'
 y_axis_title = 'Absolute frequenz\n'
 
 bar_group_names = {'pbv' : 'PBV\n(Medien)', 'pab' : 'PAB\n(Wirtschaft)', 'peb' : 'PEB\n(Wissenschaft)', 'pfu' : 'PFU\n(Politik)'} # For languages: {'fr' : 'Französisch', 'de' : 'Deutsch'...}
+
 #------------------------------------
 # To time the script
 from datetime import datetime
@@ -48,10 +49,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 #--------------------------
 
-# 2) Bar plot
-# 313342220
+# Makes the bar plot
 
-# One bar
+# For one bar group
 '''
 labels = ['Bildung', 'Fachzeitung', 'Newsdienste', 'Tageszeitung', 'Wochenzeitung']
 y = [1.28, 5.34, 15.76, 68.09, 9.50]
@@ -61,33 +61,29 @@ barlist = plt.bar(x, y, align='center')
 # bar options: color='b', edgecolor =, hatch = patterns = [ "/" , "\\" , "|" , "-" , "+" , "x", "o", "O", ".", "*" ]
 '''
 
-# Multiple bars
+# For multiple bar groups
 ax = plt.subplot(111)
-dist = 0.31
+#ax.bar([x + dist for x in range(len(dic1))], dic1.values(),width=0.3, align='center', color='#b9e1b4') 
 
 # Sets number of bar groups
-xlist = []; n = 2; xlist.append(n)
+xlist = []; dist = 0.31; n = 2; xlist.append(n)
 for i in range(1, number_of_bar_groups):
 	n = n + 3
 	xlist.append(n)
 #xlist = [2, 5, 8, 11]
-def make_bar(dic, dist, bar_color):
+def make_bars(dic, dist, bar_color):
 	ax.bar([x + dist for x in xlist], dic.values(),width=0.3, align='center', color=bar_color) 
 	
-make_bar(dic1, -(dist*4), bar_colors[keyw1])
-make_bar(dic2, -(dist*3), bar_colors[keyw2])
-make_bar(dic3, -(dist*2), bar_colors[keyw3])
-make_bar(dic4, -(dist), bar_colors[keyw4])
-make_bar(dic5, 0, bar_colors[keyw5])
-make_bar(dic6, dist, bar_colors[keyw6])
-make_bar(dic7, dist*2, bar_colors[keyw7])
-make_bar(dic8, dist*3, bar_colors[keyw8])
-
-
-#ax.bar([x + dist for x in range(len(dic1))], dic1.values(),width=0.3, align='center', color='#b9e1b4') 
+make_bars(dic1, -(dist*4), bar_colors[keyw1])
+make_bars(dic2, -(dist*3), bar_colors[keyw2])
+make_bars(dic3, -(dist*2), bar_colors[keyw3])
+make_bars(dic4, -(dist), bar_colors[keyw4])
+make_bars(dic5, 0, bar_colors[keyw5])
+make_bars(dic6, dist, bar_colors[keyw6])
+make_bars(dic7, dist*2, bar_colors[keyw7])
+make_bars(dic8, dist*3, bar_colors[keyw8])
 
 # Set x ticks
-
 labeldic = bar_group_names
 plt.xticks(xlist, [labeldic[cl] for cl in dic1.keys()], fontsize = 14)
 
@@ -108,10 +104,9 @@ for i in range (0, len(colors)):
 #plt.xlabel('\nAnteil der Klassen im Korpus:\nPBV: 50.17%, PFU: 19.59%, PEB: 17.53%, PAB: 12.69%', fontsize = 14)
 
 # Set legend
-leg = plt.legend(loc='best', frameon=False, labels = [keyw1, keyw2, keyw3, keyw4, keyw5, keyw6, keyw7, keyw8]) # edgecolor = 'b', facecolor = 'r',
+leg = plt.legend(loc='best', frameon=False, labels = legend_names) # edgecolor = 'b', facecolor = 'r',
 for text in leg.get_texts():
 	text.set_fontsize(12)
-
 
 # Set text box
 #plt.text(4.2, 6000, 'Hauptakteure:\nTribune de Genève (4.581)\nLe Courrier (2.439)', fontsize=12)
@@ -130,7 +125,6 @@ ax.autoscale(tight=False)
 plt.show()
 
 #matplotlib.pyplot.barh(yaxis, ywidths, 1, xaxis, x_labels)
-
 
 #--------------------------
 # To time the script
